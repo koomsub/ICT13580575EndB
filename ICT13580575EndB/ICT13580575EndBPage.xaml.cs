88f02d168d41d9710dd606ba0780d5eda6c7398b
dyta <18145142+dyta@users.noreply.github.com>
2017-12-21 17:36:08 +0700

    final

---
+++ ICT13580575EndB/ICT13580575EndBPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580575EndB
+{
+    public partial class ICT13580575EndBPage : ContentPage
+    {
+        public ICT13580575EndBPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ICT13580575EndB/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using ICT13580575EndB.Models;
+
+using Xamarin.Forms;
+
+namespace ICT13580575EndB
+{
+    public partial class MainPage : ContentPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+
+			newButton.Clicked += NewButton_Clicked;
+		}
+		void LoadData()
+		{
+			productListView.ItemsSource = App.DbHelper.GetProducts();
+		}
+		protected override void OnAppearing()
+		{
+			LoadData();
+		}
+		void NewButton_Clicked(object sender, EventArgs e)
+		{
+            Navigation.PushModalAsync(new ProductNewpage());
+		}
+
+		async void Delete_Clicked(object sender, System.EventArgs e)
+		{
+			var button = sender as MenuItem;
+            var car = button.CommandParameter as Product;
+			var isOk = await DisplayAlert("ยืนยัน", "คุณต้องการลบใช่มั้ย", "ใช่", "ไม่ใช่");
+		}
+
+		void Edit_Clicked(object sender, System.EventArgs e)
+		{
+
+			var button = sender as MenuItem;
+            var car = button.CommandParameter as Product;
+            Navigation.PushModalAsync(new ProductNewpage(car));
+		}
+	}
+}
